dd1852bce1161d4a4558e91817ac714a4d2a8571
Miguel Ferrando Navalón <mfnavalon@gmail.com>
2014-05-13 20:06:01 +0200

    Initial Commit

---
+++ TheBowlingGameKata/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheBowlingGameKata
+//
+//  Created by Miguel Ferrando Navalón on 13/05/14.
+//  Copyright (c) 2014 Miguel Ferrando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFNAppDelegate class]));
+    }
+}
